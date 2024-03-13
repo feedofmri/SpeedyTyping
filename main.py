9fef50f220ace8ef31cfe4604d9c933ccb092787
Md. Rubayet Islam <77844439+feedofmri@users.noreply.github.com>
2024-03-13 17:18:23 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_WHITE)
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
     stdscr.clear()
-    stdscr.addstr("How are you?")
+    stdscr.addstr("How are you?", curses.color_pair(2))
     stdscr.refresh()
     stdscr.getkey()
     
